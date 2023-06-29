64a04e4b9d6ea6b5bf08bebee37a25580d17a213
Johel Pires <johelpires@gmail.com>
2023-06-29 16:47:34 -0400

    working on Task component

--- src/Componentes/Task.js
+++ src/Componentes/Task.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import './task.css'
 
 function Task({ tarefas }) {
-    return <div>{tarefas[0]}</div>
+    return (
+        <div className="task-container">
+            <h2>{tarefas[0]}</h2>
+
+            <div className="controls">
+                <p>edit</p>
+                <p>del</p>
+                <p>priority</p>
+            </div>
+        </div>
+    )
 }
 
 export default Task

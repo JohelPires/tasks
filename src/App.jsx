5d8bb4adc17dbb6266f29b0b83f409cb5ca6c4f4
JohelPires <johelpires@gmail.com>
2023-07-11 11:42:07 -0400

    cleaned files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Projetos from './Telas/Projetos'
+
 import Home from './Telas/Home'
-import Experiencias from './Telas/Experiencias'
-import Contato from './Telas/Contato'
+
 import Sobre from './Telas/Sobre'
 import Login from './Telas/Login'
 import { useState } from 'react'
@@ -21,9 +20,6 @@ function App() {
                     <main className="container">
                         <Routes>
                             <Route path="/" element={<Home token={token} />} />
-                            <Route path="experiencias" element={<Experiencias />} />
-                            <Route path="projetos" element={<Projetos />} />
-                            <Route path="contato" element={<Contato />} />
                             <Route path="sobre" element={<Sobre />} />
                         </Routes>
                     </main>
